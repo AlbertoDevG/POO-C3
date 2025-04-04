b90703a93f3c5322f61555cbad656b74cc4ab9d2
Alberto <albertog23p@gmail.com>
2025-04-04 15:56:38 -0600

    Alberto García
    Conexión a la db

--- POO/app.js
+++ POO/app.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 require('dotenv').config();
 const port = process.env.PORT;
+
+mongoose.connect(process.env.MONGODB_URI, {
+
+})
+.then(()=> console.log('Conexión exitosa a db'))
+.catch(err => console.error('Error de configuración', err))
 
 app.get('/', (req, res)=> res.send('¡¡Saludos es mi primer server!!'))
 app.listen(port, ()=>
