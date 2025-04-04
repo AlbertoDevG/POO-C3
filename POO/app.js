ecf89c9ee935533fb0505d64febd2e34d74a4ae7
Alberto <albertog23p@gmail.com>
2025-04-04 15:38:49 -0600

    Alberto García
    .env

--- POO/app.js
+++ POO/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 3000
+
+require('dotenv').config();
+const port = process.env.PORT;
 
 app.get('/', (req, res)=> res.send('¡¡Saludos es mi primer server!!'))
 app.listen(port, ()=>
-    console.log(`Servidor corriendo en puerto ${port}! para cerrarlo preciona Ctrl + C`)
+    console.log(`Servidor corriendo exitosamente`)
 )
