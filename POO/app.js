43c1a88267d6d00ac36c1f7a82e6d63ea178b1b1
Alberto <albertog23p@gmail.com>
2025-04-19 21:05:45 -0600

    Alberto García
    Sol. 30porLab

--- POO/app.js
+++ POO/app.js
@@ -1,87 +1,127 @@
 const express = require('express')
-const Series = require('./models/series');
-const series = require('./models/series');
-require ('./services/database') //requerimiento del servicio de db
-const app = express()
+const series= require('./models/series')
 
-require('dotenv').config();
-const port = process.env.PORT;
+require ('./services/database') //Requerimiento de servicio de la base de datos
+
+const app = express()
+require('dotenv').config()
+const port= process.env.PORT
 
 app.use(express.json())
 
-app.get('/series', async (req, res) => {
-  try {
-    const serie = await series.find()
-    res.json(serie)
+//Método GET devuelve todo
+app.get('/series',async (req, res) => {
+    try{
+        const serie= await series.find()
+        res.json(serie)
 
-  } catch (err) {
-    res.status(500).json({error: 'Error al obtener las series'})
-  }
+    } catch(err){
+        res.status(500).json({ error: 'Error al obtener las series'})
+
+    }
 })
 
-app.get('/series/:id', async (req, res) => {
-  try {
-    const serie = await series.findById(req.params.id);
-    if (!serie) {
-      return res.status(404).json({ error: 'Serie no encontrada' });
+//Método GET por ID
+app.get('/series/:id', async (req, res)=>{     
+    try{
+        const serie = await series.findById(req.params.id)
+        if(!serie){
+            return res.status(404).json({ error: 'Serie no encontrada'})
+        }
+        res.json(serie)
+    } catch(err){
+        res.status(500).json({ error: 'Error al obtener la serie'})
     }
-    res.json(serie);
-  } catch (err) {
-    res.status(500).json({ error: 'Error al obtener la serie' });
-  }
-});
+})
 
-app.post('/series', async (req, res) => {
-  try {
-    const { name, capitulos } = req.body;
-    if (!name || !capitulos) {
-      return res.status(400).json({ error: 'Faltan campos obligatorios' });
+//Método POST: crea nueva serie
+app.post('/series', async (req, res)=>{
+    try{
+        const {name, capitulos, categorias, clasificacion, formato, duracion, fechaIngreso } = req.body
+
+        if(!name || !capitulos || !categorias || !clasificacion || !formato || !duracion || !fechaIngreso){
+            return res.status(400).json({ error: 'Faltan campos obligatorios'})
+
+        }
+        const nuevaSerie = new series({
+            name,
+            capitulos,
+            categorias,
+            clasificacion,
+            formato,
+            duracion,
+            fechaIngreso
+        })
+
+        const serieGuardada = await nuevaSerie.save()
+        res.status(201).json(serieGuardada)
+    }catch(err){
+        res.status(500).json({ error: 'Error al crear la serie'})
+
     }
-    const nuevaSerie = new series({
-      name,
-      capitulos
-    });
-    const serieGuardada = await nuevaSerie.save();
-    res.status(201).json(serieGuardada);
-  } catch (err) {
-    res.status(500).json({ error: 'Error al crear la serie' });
-  }
-});
+})
 
+//Método PUT: actualiza la información de una serie existente según id
+app.put('/series/:id', async (req, res)=>{
+    try{
+        const {name, capitulos, categorias, clasificacion, formato, duracion, fechaIngreso } = req.body
 
-app.put('/series/:id', async (req, res) => {
-  try {
-    const { name, capitulos } = req.body;
-    if (!name || !capitulos) {
-      return res.status(400).json({ error: 'Faltan campos obligatorios' });
+        if(!name || !capitulos || !categorias || !clasificacion || !formato || !duracion || !fechaIngreso){ //valida que todos los campos sean verdaderos
+            return res.status(400).json({ error: 'Faltan campos obligatorios'})
+
+        }
+        const serieActualizada = await series.findByIdAndUpdate(
+            req. params.id,
+            {name, capitulos, categorias, clasificacion, formato, duracion, fechaIngreso },
+            {new: true}
+        )
+
+        if(!serieActualizada){
+            return res.status(404).json({ error: 'Serie no encontrada'})
+        }
+        res.json(serieActualizada)
+    }catch(err){
+        res.status(500).json({ error: 'Error al actualizar la serie'})
+
     }
-    const serieActualizada = await series.findByIdAndUpdate(
-      req.params.id,
-      { name, capitulos },
-      { new: true }
-    );
-    if (!serieActualizada) {
-      return res.status(404).json({ error: 'Serie no encontrada' });
-    }
-    res.json(serieActualizada);
-  } catch (err) {
-    res.status(500).json({ error: 'Error al actualizar la serie' });
-  }
-});
+})
 
-app.delete('/series/:id', async (req, res) => {
-  try {
-    const serieEliminada = await series.findByIdAndDelete(req.params.id);
-    if (!serieEliminada) {
-      return res.status(404).json({ error: 'Serie no encontrada' });
-    }
-    res.json({ message: 'Serie eliminada correctamente' });
-  } catch (err) {
-    res.status(500).json({ error: 'Error al eliminar la serie' });
-  }
-});
+//Metodo DELETE: elimina 
+app.delete('/series/:id', async ( req, res) => {
+    try { 
+        const serieEliminada = await series.findByIdAndDelete(req.params.id)
 
+        if(!serieEliminada){
+            return res.status(404).json({ error: 'Serie no encontrada'})
+        }
+        res.json({ message: 'Serie eliminada correctamente'})
+    }catch(err){
+        res.status(500).json({ error: 'Error al eliminar la serie'})
+
+    }
+
+}) 
+
+//Método PATCH: actualiza un campo en especifico
+app.patch('/series/:id', async (req, res)=>{
+    try{
+        const campoActualizado= await series.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new: true}
+        )
+
+        if(!campoActualizado) {
+            return res.status(404).json({ error: 'Serie no encontrada'})
+
+        }
+
+        res.json(campoActualizado)
+    }catch(err){
+        res.status(500).json({ error: 'Error al actualizar el campo deseado'})
+    }
+})
 
 app.listen(port, ()=>
-    console.log(`Servidor corriendo exitosamente`)
+    console.log("Servidor corriendo exitosamente")
 )
--- POO/models/series.js
+++ POO/models/series.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose')
 
-const seriesSchema = new mongoose.Schema({
+const seriesSchema= new mongoose.Schema({
     name: {type: String, required: true},
-    capitulos: {type: String, required: true}
+    capitulos: {type: Number, required: true},
+    categorias: {type: [String], required: true}, //guarda los datos en un array
+    clasificacion: {type: String, required: true},
+    formato: {type: String, required: true},
+    duracion: {type: String, required: true},
+    fechaIngreso: {type: Date, required: true} //Date pq nos pide una fecha
 })
 
-module.exports = mongoose.model('series', seriesSchema)
+module.exports= mongoose.model('series', seriesSchema)
