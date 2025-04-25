ed64cde62e7576f9fa5ec86e03cf3cfd84a17c76
Alberto <albertog23p@gmail.com>
2025-04-25 09:13:28 -0600

    Alberto García
    POO

--- POO/models/series.js
+++ POO/models/series.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose')
 const seriesSchema= new mongoose.Schema({
     name: {type: String, required: true},
     capitulos: {type: Number, required: true},
-    categorias: {type: [String], required: true}, //guarda los datos en un array
+    categorias: {type: [String], required: true}, 
     clasificacion: {type: String, required: true},
     formato: {type: String, required: true},
     duracion: {type: String, required: true},
-    fechaIngreso: {type: Date, required: true} //Date pq nos pide una fecha
+    fechaIngreso: {type: Date, required: true} 
 })
 
 module.exports= mongoose.model('series', seriesSchema)
